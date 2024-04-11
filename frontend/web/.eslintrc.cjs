49c12abce9485365600a73df50f96cd216b325a4
오민상 <flos9537@gmail.com>
2024-04-12 08:50:00 +0900

    [ALL] feat: web 초기 환경설정
    prettier 추가, airbnb styleguide eslint 규칙 추가

---
+++ frontend/web/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true, es2020: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest', sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': ['error', {}, {
+      usePrettierrc: true,
+    }],
+  },
+};
