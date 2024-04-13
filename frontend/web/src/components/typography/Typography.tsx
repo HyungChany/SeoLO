d44db20e67cf001896c1ac00e47d6c2882d27a32
오민상 <flos9537@gmail.com>
2024-04-13 19:57:16 +0900

    [FE] feat: 폰트 추가

---
+++ frontend/web/src/components/typography/Typography.tsx
@@ -0,0 +1,224 @@
+import * as FontStyle from '@/config/fontStyle/fontStyle.ts';
+import styled from 'styled-components';
+
+interface TypographyProps {
+  color?: string;
+  children: React.ReactNode;
+}
+
+interface FontProps extends TypographyProps {
+  fontFamily: string;
+  fontSize: number;
+}
+
+const StyledHeaderTypography = styled.div<FontProps>`
+  font-family: ${(props) => props.fontFamily};
+  font-size: ${(props) => `${props.fontSize}px`};
+  color: ${(props) => props.color || 'black'};
+  line-height: ${(props) => `${props.fontSize * 1.5}px`};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const StyledTypography = styled.div<FontProps>`
+  font-family: ${(props) => props.fontFamily};
+  font-size: ${(props) => `${props.fontSize}px`};
+  color: ${(props) => props.color || 'black'};
+  line-height: ${(props) => `${props.fontSize * 1.5}px`};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const H1 = (props: TypographyProps) => {
+  return (
+    <StyledHeaderTypography
+      fontFamily={FontStyle.F_PRIMARY_EB}
+      fontSize={FontStyle.F_SIZE_XXXXL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledHeaderTypography>
+  );
+};
+
+export const H2 = (props: TypographyProps) => {
+  return (
+    <StyledHeaderTypography
+      fontFamily={FontStyle.F_PRIMARY_EB}
+      fontSize={FontStyle.F_SIZE_XXXL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledHeaderTypography>
+  );
+};
+
+export const H3 = (props: TypographyProps) => {
+  return (
+    <StyledHeaderTypography
+      fontFamily={FontStyle.F_PRIMARY_EB}
+      fontSize={FontStyle.F_SIZE_XXL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledHeaderTypography>
+  );
+};
+
+export const H4 = (props: TypographyProps) => {
+  return (
+    <StyledHeaderTypography
+      fontFamily={FontStyle.F_PRIMARY_EB}
+      fontSize={FontStyle.F_SIZE_XL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledHeaderTypography>
+  );
+};
+
+export const Body0B = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_B}
+      fontSize={FontStyle.F_SIZE_XXXL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body0M = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_XXXL}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body1B = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_B}
+      fontSize={FontStyle.F_SIZE_L}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body1M = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_L}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body2B = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_B}
+      fontSize={FontStyle.F_SIZE_M}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body2M = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_M}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body3B = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_B}
+      fontSize={FontStyle.F_SIZE_S}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body3M = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_S}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body4B = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_B}
+      fontSize={FontStyle.F_SIZE_XS}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Body4M = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_XS}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Detail0 = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_XXS}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
+
+export const Detail1 = (props: TypographyProps) => {
+  return (
+    <StyledTypography
+      fontFamily={FontStyle.F_PRIMARY_M}
+      fontSize={FontStyle.F_SIZE_XXXS}
+      color={props.color}
+    >
+      {props.children}
+    </StyledTypography>
+  );
+};
---
+++ frontend/web/src/config/fontStyle/fontStyle.ts
@@ -0,0 +1,16 @@
+// font-size
+export const F_SIZE_XXXXL: number = 40;
+export const F_SIZE_XXXL: number = 30;
+export const F_SIZE_XXL: number = 28;
+export const F_SIZE_XL: number = 24;
+export const F_SIZE_L: number = 22;
+export const F_SIZE_M: number = 20;
+export const F_SIZE_S: number = 18;
+export const F_SIZE_XS: number = 16;
+export const F_SIZE_XXS: number = 14;
+export const F_SIZE_XXXS: number = 12;
+
+// font-family
+export const F_PRIMARY_EB: string = 'NYJGothicEB';
+export const F_PRIMARY_B: string = 'NYJGothicB';
+export const F_PRIMARY_M: string = 'NYJGothicM';
