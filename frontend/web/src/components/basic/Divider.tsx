4dd3f4019b9fa03df89da3ff1bff59592730ef3b
오민상 <flos9537@gmail.com>
2024-04-21 12:53:23 +0900

    [FE] feat: 스페이서, 구분선 추가

---
+++ frontend/web/src/components/basic/Divider.tsx
@@ -0,0 +1,41 @@
+import * as Color from '../../config/color/Color';
+
+interface DividerProps {
+  borderWidth?: number;
+  marginHorizontal?: number;
+  borderColor?: string;
+}
+
+/**
+ * div와 같은 컴포넌트를 구분짓는 divider입니다.
+ *
+ * @returns 컴포넌트의 스타일은 props를 통해 조절 가능합니다:
+ *   - borderWidth (기본값: 0.05rem): Divider의 선 두께를 설정합니다.
+ *   - marginHorizontal (기본값: 1rem): 좌우 마진을 설정합니다.
+ *   - borderColor (기본값: Color.GRAY300): 선의 색상을 설정합니다.
+ *
+ * ### 사용 예시
+ *
+ * ```tsx
+ * <Divider />
+ * <Divider borderWidth={0.1} marginHorizontal={2} borderColor="Color.GRAY300" />
+ * <Divider borderWidth={0.2} marginHorizontal={3} borderColor="#Color.RED100" />
+ * ```
+ *
+ * 각 Divider는 페이지 내 다른 섹션들을 구분하는 데 사용할 수 있습니다.
+ * @author 오민상
+ */
+export const Divider = (props: DividerProps) => {
+  return (
+    <div
+      style={{
+        alignSelf: 'stretch',
+        borderWidth: `${props.borderWidth || 0.05}rem`,
+        marginLeft: `${props.marginHorizontal || 1}rem`,
+        marginRight: `${props.marginHorizontal || 1}rem`,
+        borderColor: props.borderColor || Color.GRAY300,
+        borderStyle: 'solid',
+      }}
+    />
+  );
+};
---
+++ frontend/web/src/components/basic/Spacer.tsx
@@ -0,0 +1,38 @@
+interface SpacerProps {
+  space: string;
+  horizontal?: boolean;
+}
+
+/**
+ * div의 margin을 선언한 컴포넌트입니다. 좀 더 편한 space를 위합니다.
+ * 이 컴포넌트는 주로 레이아웃 내에서 요소 간의 간격을 조절할 때 사용됩니다.
+ *
+ * @param props 컴포넌트 설정을 위한 프로퍼티:
+ * - `space`: 마진의 크기를 설정합니다. CSS 단위를 포함한 문자열로 지정하세요 (예: "1rem", "20px").
+ * - `horizontalDirection`: `true`일 경우, 좌우 마진(`marginLeft`)을 적용하고, `false` 또는 생략 시 상단 마진(`marginTop`)을 적용합니다.
+ *
+ * ### 사용 예시
+ *
+ * 수직 공간 추가:
+ * ```jsx
+ * <Spacer space="2rem" />
+ * ```
+ *
+ * 수평 공간 추가:
+ * ```jsx
+ * <Spacer space="50px" horizontal={true} />
+ * ```
+ *
+ * 각 Spacer는 페이지 내 다른 섹션들을 구분하거나, 요소 간의 공간을 추가하는 데 유용합니다.
+ *
+ * @returns JSX.Element - 설정된 마진을 갖는 div 요소를 반환합니다.
+ * @author 오민상
+ */
+
+export const Spacer = (props: SpacerProps) => {
+  if (props.horizontal) {
+    return <div style={{ marginLeft: props.space }} />;
+  }
+
+  return <div style={{ marginTop: props.space }} />;
+};
