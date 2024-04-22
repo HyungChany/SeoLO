2618fdbfae6f13ce44bb2977ad4cdc407e7557d5
오유진 <yjin0414@naver.com>
2024-04-22 15:33:10 +0900

    [BE]feat: Django 초기설정

---
+++ backend/news/my_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'django-insecure-^tp1k7xy*-axtcnhqze2yo&waew$el7!6oka6f$&1hk=(##_t+'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
