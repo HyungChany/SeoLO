40a42b1b3b1bb7496cc54826b129b7aefa3859d8
tlfahd2 <tlfahd2@naver.com>
2024-04-22 16:34:15 +0900

    [FE] feat: 잠금화면 구성

--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screen_lock/flutter_screen_lock.dart';
+import 'package:local_auth/local_auth.dart';
+
+const Color blue100 = Color.fromRGBO(135, 185, 231, 1);
+const Color blue800 = Color.fromRGBO(56, 61, 101, 1);
+const Color mint200 = Color.fromRGBO(207, 241, 214, 1);
+const Color samsungBlue = Color.fromRGBO(20, 40, 160, 1);
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized(); // 초기화 보장
+  WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized(); // 초기화 보장
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
@@ -10,38 +18,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SeoLo',
+      // debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   void initState() {
     super.initState();
@@ -52,32 +49,90 @@ class _MyHomePageState extends State<MyHomePage> {
     FlutterNativeSplash.remove();
   }
 
+  Future<void> localAuth(BuildContext context) async {
+    final localAuth = LocalAuthentication();
+    final didAuthenticate = await localAuth.authenticate(
+        localizedReason: 'Please authenticate',
+        options: const AuthenticationOptions(biometricOnly: true));
+    if (didAuthenticate) {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 700,
+              ),
+              child: Wrap(
+                spacing: 16,
+                runSpacing: 16,
+                alignment: WrapAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      screenLockCreate(
+                        context: context,
+                        title: const Text('암호를 입력해주세요.'),
+                        confirmTitle: const Text('암호를 다시 입력해주세요.'),
+
+                        // 5번 틀리면 60초동안 입력 금지
+                        maxRetries: 5,
+                        retryDelay: const Duration(seconds: 60),
+                        delayBuilder: (context, delay) => Text(
+                          '${(delay.inMilliseconds / 1000).ceil()}초 뒤에 입력 가능합니다.',
+                        ),
+
+                        // 지문인식
+                        customizedButtonChild: const Icon(
+                          Icons.fingerprint,
+                        ),
+                        customizedButtonTap: () async =>
+                            await localAuth(context),
+                        onOpened: () async => await localAuth(context),
+
+                        onConfirmed: (value) => Navigator.of(context).pop(),
+                        config: const ScreenLockConfig(
+                          backgroundColor: blue100,
+                          // titleTextStyle: TextStyle(fontSize: 24),
+                        ),
+                        secretsConfig: SecretsConfig(
+                          spacing: 15, // or spacingRatio
+                          padding: const EdgeInsets.all(40),
+                          secretConfig: SecretConfig(
+                            disabledColor: Colors.grey,
+                            enabledColor: blue100,
+                            size: 15,
+                            builder: (context, config, enabled) => Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: enabled
+                                    ? config.enabledColor
+                                    : config.disabledColor,
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              width: config.size,
+                              height: config.size,
+                            ),
+                          ),
+                        ),
+                        keyPadConfig: const KeyPadConfig(
+                          // 길게 누르면 전체 삭제
+                          clearOnLongPressed: true,
+                        ),
+                      );
+                    },
+                    child: const Text('잠금화면'),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
