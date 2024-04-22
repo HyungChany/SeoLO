33b1e550999bcf7e5f9b0e866aa5e401a5d19a30
오정민 <dhwjdals1324@gmail.com>
2024-04-22 17:15:51 +0900

    feat : navigator 컴포넌트 기능 제외 구현 완료

--- frontend/web/src/components/navigation/navigator.tsx
+++ frontend/web/src/components/navigation/navigator.tsx
@@ -7,8 +7,6 @@ const NavigatorBox = styled.div`
   width: 100%;
   height: 7.5rem;
   padding: 2rem 2.65625rem;
-  align-items: center;
-  justify-content: center;
   display: flex;
   top: 0;
   background-color: ${Color.SNOW};
@@ -16,25 +14,41 @@ const NavigatorBox = styled.div`
 `;
 const NavigatorContent = styled.div`
   display: flex;
-  /* width: 50%; */
-  width: calc(100% - 2 * 2.65625rem);
+  width: 100%;
+  /* width: calc(100% - 2 * 2.65625rem); */
+  /* height: calc(100% - 2 * 2rem); */
   align-items: center;
   gap: 16.875rem;
 `;
 const Title = styled.div`
   width: 11.125rem;
-  height: 3.5rem;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
   color: ${Color.BLUE100};
   font-size: 3rem;
   font-style: normal;
   font-weight: 700;
   display: flex;
 `;
+const Menu = styled.div`
+  width: 100%;
+  margin-right: 1.62rem;
+  gap: 9.38rem;
+  /* justify-content: space-between; */
+  display: flex;
+  flex-direction: row;
+`;
 export const TopNavigator = () => {
   return (
     <NavigatorBox>
       <NavigatorContent>
         <Title>SeoLo</Title>
+        <Menu>
+          <Typo.H3>LOTO 일지</Typo.H3>
+          <Typo.H3>체크리스트 관리</Typo.H3>
+          <Typo.H3>사업장 정보조회/수정</Typo.H3>
+        </Menu>
       </NavigatorContent>
     </NavigatorBox>
   );
