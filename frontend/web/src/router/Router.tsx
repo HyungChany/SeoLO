b4cdcccb889d62d3d94bff0a00d95dbdfef65da2
kkongch <dhwjdals1324@gmail.com>
2024-04-23 21:04:51 +0900

    fix : Router 코드 수정 및 app.tsx 개편

--- frontend/web/src/router/Router.tsx
+++ frontend/web/src/router/Router.tsx
@@ -1,10 +1,9 @@
-import CompanyInformation from '@/pages/CompanyInformation';
+import { Example } from '@/components/Example.tsx';
+import CompanyInformation from '@/pages/CompanyInformation.tsx';
 import { createBrowserRouter } from 'react-router-dom';
-
-const checkRoutes = [
-  { path: '/companycheck', element: <CompanyInformation /> },
-];
-const routes = [...checkRoutes];
+const mainRoutes = [{ path: '/', element: <Example /> }];
+const checkRoutes = [{ path: '/information', element: <CompanyInformation /> }];
+const routes = [...checkRoutes, ...mainRoutes];
 const Router = createBrowserRouter(routes);
-//
+
 export default Router;
