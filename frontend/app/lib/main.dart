bb487157d2d752c5ef60211017c19b1962f0a4a4
tlfahd2 <tlfahd2@naver.com>
2024-04-23 13:52:27 +0900

    [FE] test: API 연결 테스트

--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app/routes/main_route.dart';
 import 'package:app/screens/lock/lock_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 const Color blue100 = Color.fromRGBO(135, 185, 231, 1);
@@ -9,11 +10,12 @@ const Color mint200 = Color.fromRGBO(207, 241, 214, 1);
 const Color gray100 = Color.fromRGBO(242, 244, 247, 1);
 const Color samsungBlue = Color.fromRGBO(20, 40, 160, 1);
 
-void main() {
+void main() async{
   // WidgetsBinding widgetsBinding =
       WidgetsFlutterBinding.ensureInitialized(); // 초기화 보장
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const MyApp());
+      await dotenv.load(fileName: '.env');
+      runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
