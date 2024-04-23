d5405a2fe752a91e36f3fff1dc8ec249fcb5507d
오유진 <yjin0414@naver.com>
2024-04-23 23:53:06 +0900

    [BE]feat: Scraping 코드 작성

--- backend/news/scraper/scraping.py
+++ backend/news/scraper/scraping.py
@@ -1,10 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_data():
-    url = ''
+def scrape():
+    url = 'https://search.daum.net/search?w=news&nil_search=btn&DA=STC&enc=utf8&cluster=y&cluster_page=1&q=%EC%95%88%EC%A0%84&p=1&sort=accuracy'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    
-    print(soup.title.text)
 
+    articles_list = soup.find('ul', class_='c-list-basic')
+
+    articles = articles_list.find_all('li')
+
+    for article in articles[:5]:
+        thumbnail_tag = article.find('div', class_='item-thumb').find('img')
+        thumbnail = thumbnail_tag['data-original-src'] if thumbnail_tag else None
+        
+        source = article.find('span', class_='txt_info').text.strip()
+        
+        content_div = article.find('div', class_='item-title')
+        title = content_div.find('a').text.strip()
+        link = content_div.find('a')['href']
+
+        preview = article.find('div', class_='item-contents').find('p').text.strip()
+
+        date = article.find('span', class_='gem-subinfo').find('span').text.strip()
+
+        # 결과 출력
+        # print("제목:", title)
+        # print("프리뷰:", preview)
+        # print("링크:", link)
+        # print("썸네일:", thumbnail)
+        # print("시간:", date)
+        # print("신문사:", source)
+        # print()
+
+scrape()
