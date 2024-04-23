25b2c5040578258e13f6e902b6ecffb5c7532c24
오민상 <flos9537@gmail.com>
2024-04-23 11:41:16 +0900

    [FE] fix: port 번호 수정

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   base: './',
   server: {
+    port: 5173,
     host: '0.0.0.0',
   },
   plugins: [react(), svgr()],
