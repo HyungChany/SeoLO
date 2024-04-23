3501f40bf8bd60a533ade2c744d7453abf5879e2
오유진 <yjin0414@naver.com>
2024-04-23 15:56:06 +0900

    [BE]feat: CheckListEntity 추가

---
+++ backend/seolo/src/main/java/com/c104/seolo/domain/checklist/entity/CheckList.java
@@ -0,0 +1,30 @@
+package com.c104.seolo.domain.checklist.entity;
+
+import com.c104.seolo.domain.company.entity.Company;
+import com.c104.seolo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class CheckList extends BaseEntity {
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "check_list_id")
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(name = "check_list_context", length = 255)
+    private String checkListContext;
+}
