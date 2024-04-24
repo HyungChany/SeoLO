b5b7cb84ee127923a65249a286177cbf2dd99b01
오유진 <yjin0414@naver.com>
2024-04-24 22:37:05 +0900

    [BE]init: Custom Header 위한 ãInterceptor

---
+++ backend/seolo/src/main/java/com/c104/seolo/global/config/MvcConfig.java
@@ -0,0 +1,24 @@
+package com.c104.seolo.global.config;
+
+import com.c104.seolo.global.common.CCodeInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(cCodeInterceptor())
+                .addPathPatterns("/**") // 모든 URL에 인터셉터를 적용
+                .excludePathPatterns("/public/**"); // 예외 처리할 URL 패턴 지정
+    }
+
+    @Bean
+    public CCodeInterceptor cCodeInterceptor() {
+        return new CCodeInterceptor();
+    }
+}
