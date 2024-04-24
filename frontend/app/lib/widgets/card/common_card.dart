406a970f3c28df8fb48fd06be6c4287aa26c5d78
오정민 <dhwjdals1324@gmail.com>
2024-04-24 20:17:48 +0900

    fix : AppCard 컴포넌트 수정

--- frontend/app/lib/widgets/card/common_card.dart
+++ frontend/app/lib/widgets/card/common_card.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import '../../main.dart';
 
 class CommonCard extends StatefulWidget {
-  final double width; // 너비
-  final double height; // 높이
 
-  const CommonCard({
-    super.key,
-    this.width = 300.0,
-    this.height = 45.0,
-  });
+  const CommonCard({super.key});
 
   @override
   _CommonCardState createState() => _CommonCardState();
@@ -17,14 +10,25 @@ class CommonCard extends StatefulWidget {
 class _CommonCardState extends State<CommonCard> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    // 화면 크기에 따라 width와 height를 설정합니다.
+    // 예를 들어, 화면 너비의 30%와 높이의 20%를 사용합니다.
+    double width = screenWidth * 0.3;
+    double height = screenHeight * 0.2;
     return Container(
-      width: widget.width,
-      height: widget.height,
+      width: width,
+      height: height,
       decoration: BoxDecoration(
-        color: Color.fromRGBO(135, 185, 231, 0.5), // 컨테이너 배경색
+        color: Color.fromRGBO(237, 244, 251, 1), // 컨테이너 배경색
         borderRadius: BorderRadius.circular(10.0), // 모서리 둥글기
-        boxShadow: [
-          shadow
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.25), // 그림자 확산 범위
+            blurRadius: 5, // 그림자 흐림 정도
+            offset: Offset(0, 2), // 그림자 위치
+          ),
         ],
       ),
       child: const Column(
