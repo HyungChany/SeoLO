9ddd28ce6e30a83dbb920c61890dd3b2596b6a74
tlfahd2 <tlfahd2@naver.com>
2024-04-25 13:30:32 +0900

    [FE] feat: header widget 생성

---
+++ frontend/app/lib/widgets/header/header.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool back;
+
+  const Header({super.key, required this.title, required this.back});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.white,
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold),),
+      centerTitle: true,
+      leading: back ? IconButton(
+        icon: const Icon(Icons.arrow_back_ios_new),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ) : null,
+    );
+  }
+
+}
