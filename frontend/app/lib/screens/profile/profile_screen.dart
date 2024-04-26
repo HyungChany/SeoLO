57e086ba622b865c82087380431c166f9102cbe9
tlfahd2 <tlfahd2@naver.com>
2024-04-26 15:25:27 +0900

    [FE] feat: p my iormation widget 생성

--- frontend/app/lib/screens/profile/profile_screen.dart
+++ frontend/app/lib/screens/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app/widgets/header/header.dart';
 import 'package:app/widgets/profile/logout_button.dart';
+import 'package:app/widgets/profile/my_info.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,9 +14,17 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:Header(title: '프로필', back: false),
-      body: Row(
-        children: [LogoutButton()
+      appBar: Header(title: '프로필', back: false),
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.1,
+            child: Row(
+              children: [MyInfo(), LogoutButton()],
+            ),
+          ),
+          Divider(color: Colors.grey, thickness: 1.0,)
         ],
       ),
     );
