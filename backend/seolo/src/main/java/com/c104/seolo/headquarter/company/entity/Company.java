c94e34d646cab199df0653fb390bf5a01f42f3f3
김진명 <willom2c@naver.com>
2024-04-27 16:58:44 +0900

    [BE] feat : Company Dummy 1. Create a Script to make company dummy 2. Set the script to run every time you do the Spring server in the H2 DB env

--- backend/seolo/src/main/java/com/c104/seolo/headquarter/company/entity/Company.java
+++ backend/seolo/src/main/java/com/c104/seolo/headquarter/company/entity/Company.java
@@ -15,7 +15,7 @@ import lombok.ToString;
 public class Company extends BaseEntity {
     // 외부 DB에서 불러온다는 가정하에 생성자 생성안했음
     @Id
-    @Column(name = "company_code", length = 9 , nullable = false)
+    @Column(name = "company_code", length = 9 , nullable = false, columnDefinition = "CHAR(9)")
     private String companyCode;
 
     @Column(name = "company_name", nullable = false ,length = 60)
@@ -24,10 +24,10 @@ public class Company extends BaseEntity {
     @Column(name = "company_logo", nullable = false, length = 255)
     private String companyLogo;
 
-    @Column(name = "company_registration_num", nullable = false, length = 255)
+    @Column(name = "company_registration_num", nullable = false, length = 12, columnDefinition = "CHAR(12)")
     private String companyRegistrationNum;
 
-    @Column(name = "company_accident_manage_num", nullable = false, length = 255)
+    @Column(name = "company_accident_manage_num", nullable = false, length = 14, columnDefinition = "CHAR(14)")
     private String companyAccidentManageNum;
 
     // JPA 프록시 객체 생성을 위한 기본생성자
