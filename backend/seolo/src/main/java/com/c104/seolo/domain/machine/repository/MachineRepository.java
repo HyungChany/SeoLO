dd544878080a6b42c4bea44b8dec117cf94c325e
오유진 <yjin0414@naver.com>
2024-04-29 22:06:04 +0900

    [BE]feat: 작업장별 장비조회 Repository

--- backend/seolo/src/main/java/com/c104/seolo/domain/machine/repository/MachineRepository.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/machine/repository/MachineRepository.java
@@ -13,10 +13,11 @@ import java.util.Optional;
 public interface MachineRepository extends JpaRepository<Machine, Integer> {
     @Query("SELECT new com.c104.seolo.domain.machine.dto.info.MachineListInfo( " +
             "f.id, f.facilityName, " +
-            "m.machineSubcategory.subcategory, " +
+            "sc.subcategory, " +
             "m.id, m.name, m.number, m.createdAt, " +
             "mm.user.id, mm.user.employee.employeeName, mm.mm_role " +
             ") FROM Machine m " +
+            "JOIN m.machineSubcategory sc " +
             "JOIN m.facility f " +
             "LEFT JOIN MachineManager mm ON m.id = mm.machine.id " +
             "WHERE f.id = :facilityId AND f.company.companyCode = :companyCode")
