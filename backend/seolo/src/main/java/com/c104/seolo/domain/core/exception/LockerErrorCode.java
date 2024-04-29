500d4bea444bf10ebc3b6ebb56a821402e697ed2
오유진 <yjin0414@naver.com>
2024-04-29 17:36:00 +0900

    [BE]style: ErrorCodeStyle

--- backend/seolo/src/main/java/com/c104/seolo/domain/core/exception/LockerErrorCode.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/core/exception/LockerErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum LockerErrorCode {
-    NOT_EXIST_LOCKER("자물쇠를 조회할 수 없습니다.", "CT01", HttpStatus.BAD_REQUEST);
+    NOT_EXIST_LOCKER("자물쇠를 조회할 수 없습니다.", "LK01", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final String errorCode;
