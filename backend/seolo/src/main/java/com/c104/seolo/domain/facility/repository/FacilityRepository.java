8605a98336d1bd8e476fd94fde46fe439e8a2e15
오유진 <yjin0414@naver.com>
2024-04-29 12:07:13 +0900

    [BE]feat: Facility Repository

--- backend/seolo/src/main/java/com/c104/seolo/domain/facility/repository/FacilityRepository.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/facility/repository/FacilityRepository.java
@@ -1,14 +1,23 @@
 package com.c104.seolo.domain.facility.repository;
 
 import com.c104.seolo.domain.facility.dto.info.FacilityInfo;
+import com.c104.seolo.domain.facility.entity.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class FacilityRepository {
-//    @Query("select new com.c104.seolo.domain.facility.dto.info.FacilityInfo( " +
-//            "f.id, f.facilityName, f.facilityAddress, f.facilityLayout, f.facilityThum ) from Facility f")
-//    List<FacilityInfo> getFacilities();
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
+    @Query("select new com.c104.seolo.domain.facility.dto.info.FacilityInfo( " +
+            "f.id, f.facilityName, f.facilityAddress, f.facilityLayout, f.facilityThum ) from Facility f " +
+            "where f.company.companyCode = :company_code")
+    List<FacilityInfo> getFacilities(String company_code);
+
+    @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM Facility f " +
+            "WHERE f.company.companyCode = :company_code")
+    boolean existsByCompany(@Param("company_code") String company_code);
+
 }
