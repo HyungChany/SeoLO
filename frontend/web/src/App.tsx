20c503f7490c2705a9acb4ee1b8c1efda1638403
오정민 <dhwjdals1324@gmail.com>
2024-04-29 10:22:15 +0900

    fix : font폴더 구조 수정

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { RecoilRoot } from 'recoil';
-import './font/fonts.css';
+import '/font/font.css';
 import Router from './router/Router.tsx';
 import { RouterProvider } from 'react-router-dom';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
