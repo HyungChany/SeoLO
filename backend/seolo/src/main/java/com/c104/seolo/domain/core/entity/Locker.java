25aed7f9de684f80c15e0798626f092067d07132
오유진 <yjin0414@naver.com>
2024-04-29 16:20:50 +0900

    [BE]feat: Locker Column 추가

--- backend/seolo/src/main/java/com/c104/seolo/domain/core/entity/Locker.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/core/entity/Locker.java
@@ -21,19 +21,29 @@ public class Locker extends BaseEntity {
     @JoinColumn(name = "company_code", referencedColumnName = "company_code", nullable = false)
     private Company company;
 
+    @Column(name = "locker_uid", length = 8, nullable = false)
+    private String uid;
+
     @Column(name = "locker_isLocked")
     private boolean isLocked;
+
+    @Column(name = "locker_battery", nullable = false, columnDefinition = "BOOLEAN DEFAULT 100")
+    private int battery;
 
     protected Locker () {}
 
     public Locker(Builder builder) {
         this.company = builder.company;
+        this.uid = builder.uid;
         this.isLocked = builder.isLocked;
+        this.battery = builder.battery;
     }
 
     public static class Builder {
         private Company company;
+        private String uid;
         private boolean isLocked = false;
+        private int battery;
 
         public Builder company(Company company) {
             if (company == null) {
@@ -43,8 +53,21 @@ public class Locker extends BaseEntity {
             return this;
         }
 
+        public Builder uid(String uid) {
+            if (uid == null) {
+                throw new IllegalArgumentException("Uid cannot be null");
+            }
+            this.uid = uid;
+            return this;
+        }
+
         public Builder isLocked(boolean isLocked) {
             this.isLocked = isLocked;
+            return this;
+        }
+
+        public Builder battery(int battery) {
+            this.battery = battery;
             return this;
         }
 
