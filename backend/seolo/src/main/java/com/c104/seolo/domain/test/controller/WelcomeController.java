6e748ebd36c96722f106481ad971e0b6bb4da2eb
오유진 <yjin0414@naver.com>
2024-04-30 09:45:15 +0900

    [BE]remove: remove

--- backend/seolo/src/main/java/com/c104/seolo/domain/test/controller/WelcomeController.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/test/controller/WelcomeController.java
@@ -1,15 +1,7 @@
 package com.c104.seolo.domain.test.controller;
 
-import com.c104.seolo.domain.facility.dto.response.FacilityResponse;
-import com.c104.seolo.domain.facility.service.FacilityService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-=======
->>>>>>> 56b3a470655d13d0946dcdfea47284bc70bb1293
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
