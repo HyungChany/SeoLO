82457118aa52501baa2e0428a0587e0b27f64ee8
오유진 <yjin0414@naver.com>
2024-04-30 10:53:45 +0900

    [BE]style: Interceptor Name 변경

--- backend/seolo/src/main/java/com/c104/seolo/domain/user/exception/UserErrorCode.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/user/exception/UserErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode {
     APPUSER_ALREADY_IN_DB("UR01","이미 가입된 회원입니다.", HttpStatus.BAD_REQUEST),
     NOT_EXIST_APPUSER("UR02","아이디 또는 비밀번호가 다릅니다.",HttpStatus.BAD_REQUEST),
+    NOT_MANAGER("UR03", "관리자만 접근 가능한 경로입니다.", HttpStatus.FORBIDDEN),
     ;
 
     private final String errorCode;
--- backend/seolo/src/main/java/com/c104/seolo/domain/user/repository/UserRepository.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.c104.seolo.domain.user.repository;
 
 import com.c104.seolo.domain.user.entity.AppUser;
+import com.c104.seolo.domain.user.enums.ROLES;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,7 @@ public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     @Query("SELECT u FROM AppUser u WHERE u.employee.employeeNum = :employeeNum")
     Optional<AppUser> findAppUserByEmployeeNum(@Param("employeeNum") String employeeNum);
+
+    @Query("SELECT u.ROLES FROM AppUser u WHERE u.employee.employeeNum = :employeeNum AND u.employee.company.companyCode = :companyCode")
+    ROLES findRoleByEmployeeNumAndCompanyCode(@Param("employeeNum") String employeeNum, @Param("companyCode") String companyCode);
 }
