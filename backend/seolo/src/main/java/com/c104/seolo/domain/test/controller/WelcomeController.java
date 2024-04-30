a2462511d6a53873cffe04f195c24611c9907004
김진명 <willom2c@naver.com>
2024-04-30 09:34:59 +0900

    [BE] fix : init sql by dev

--- backend/seolo/src/main/java/com/c104/seolo/domain/test/controller/WelcomeController.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/test/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 
-    @GetMapping("/api/test")
+    @GetMapping("/test")
     @ResponseBody
     public String sayHello() {
         return "Welcome";
