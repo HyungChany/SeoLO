fc802ad6f743af184d58c3022dd1a82fa34b06b9
오민상 <flos9537@gmail.com>
2024-05-01 16:24:34 +0900

    [fe]feat: Watch 비밀번호 입력 화면 구현

--- frontend/watch/app/src/main/java/com/seolo/seolo/presentation/PasswordActivity.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/presentation/PasswordActivity.kt
@@ -1,34 +1,17 @@
-
 package com.seolo.seolo.presentation
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.NumberPicker
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.seolo.seolo.R
 
 class PasswordActivity : AppCompatActivity() {
-    private lateinit var passcodeView: TextView
-    private lateinit var numberPicker: NumberPicker
-    private val passcode = StringBuilder("----")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setTheme(android.R.style.Theme_DeviceDefault)
         supportActionBar?.hide()
-        setContentView(R.layout.password_layout) // 오타 수정
+        setContentView(R.layout.password_layout)
 
-        passcodeView = findViewById(R.id.passcode_view)
-        numberPicker = findViewById(R.id.number_picker)
-        numberPicker.minValue = 0
-        numberPicker.maxValue = 9
-
-        findViewById<Button>(R.id.button_confirm).setOnClickListener {
-            val index = passcode.indexOf("-")
-            if (index != -1) {
-                passcode.setCharAt(index, '*')
-                passcodeView.text = passcode.toString()
-            }
-        }
     }
 }
