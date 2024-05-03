e78391a7a9d3e798e264e6d7cd021494dfde1324
오유진 <yjin0414@naver.com>
2024-05-03 21:11:39 +0900

    [BE]feat: Employee APIS

--- backend/seolo/src/main/java/com/c104/seolo/headquarter/employee/repository/EmployeeRepository.java
+++ backend/seolo/src/main/java/com/c104/seolo/headquarter/employee/repository/EmployeeRepository.java
@@ -1,12 +1,21 @@
 package com.c104.seolo.headquarter.employee.repository;
 
+import com.c104.seolo.headquarter.employee.dto.EmployeeDto;
 import com.c104.seolo.headquarter.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
     Optional<Employee> findEmployeeByEmployeeNum(String employeeNum);
+
+    @Query("SELECT new com.c104.seolo.headquarter.employee.dto.EmployeeDto(" +
+            "e.employeeNum, e.employeeName, e.employeeTitle, e.employeeTeam, e.employeeBirthday, e.employeeThum, e.employeeJoinDate, e.employeeLeaveDate " +
+            ") FROM Employee e " +
+            "WHERE e.company.companyCode = :companyCode ")
+    List<EmployeeDto> findEmployeesByCompanyCode(String companyCode);
 }
