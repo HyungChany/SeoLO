413a515e830822fae7cd9930327c3a4a617106d4
오정민 <dhwjdals1324@gmail.com>
2024-05-03 20:38:11 +0900

    fix : lock api header 수정

--- frontend/web/src/apis/Lock.ts
+++ frontend/web/src/apis/Lock.ts
@@ -2,8 +2,12 @@ import { api } from './Base.ts';
 
 export const lockCheck = async (companycode: string) => {
   try {
+    const accessToken = sessionStorage.getItem('accesstoken');
     const response = await api.get('/locks', {
-      headers: { 'Company-Code': companycode },
+      headers: {
+        'Company-Code': companycode,
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
     return response.data;
   } catch (error) {
