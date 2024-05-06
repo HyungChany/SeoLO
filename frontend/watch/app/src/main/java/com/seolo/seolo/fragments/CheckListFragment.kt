9dd03c42ee6364cfdbfe7ab6446d5dee8b9af5a1
오민상 <flos9537@gmail.com>
2024-05-06 16:05:49 +0900

    [FE] feat: 체크리스트 화면전환 및 캐러셀 동작 방식 추가

---
+++ frontend/watch/app/src/main/java/com/seolo/seolo/fragments/CheckListFragment.kt
@@ -0,0 +1,69 @@
+package com.seolo.seolo.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.seolo.seolo.R
+import com.seolo.seolo.presentation.CheckListActivity
+
+class CheckListFragment : Fragment() {
+    private var checklistText: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            checklistText = it.getString(ARG_TEXT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.checklist_item_layout, container, false)
+        val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
+        val textView = view.findViewById<TextView>(R.id.textView)
+        val textBox = view.findViewById<LinearLayout>(R.id.textbox)
+
+        textView.text = checklistText
+
+        textBox.setOnClickListener {
+            val wasChecked = checkBox.isChecked
+            checkBox.isChecked = !checkBox.isChecked
+            if (!wasChecked) {
+                view.postDelayed({
+                    (activity as? CheckListActivity)?.moveToNextPage()
+                }, 800)
+            }
+        }
+
+        view.setOnClickListener {
+            val wasChecked = checkBox.isChecked
+            checkBox.isChecked = !checkBox.isChecked
+            if (!wasChecked) {
+                view.postDelayed({
+                    (activity as? CheckListActivity)?.moveToNextPage()
+                }, 800)
+            }
+        }
+
+        return view
+    }
+
+    companion object {
+        private const val ARG_TEXT = "arg_text"
+
+        fun newInstance(text: String): CheckListFragment {
+            return CheckListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_TEXT, text)
+                }
+            }
+        }
+    }
+}
