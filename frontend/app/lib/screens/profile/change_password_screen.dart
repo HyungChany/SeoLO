6642be7cbda8c210073b9bff457054cdfab6c656
tlfahd2 <tlfahd2@naver.com>
2024-05-07 11:17:11 +0900

    [FE] fix: 비밀번호 재설정 키보드

--- frontend/app/lib/screens/profile/change_password_screen.dart
+++ frontend/app/lib/screens/profile/change_password_screen.dart
@@ -3,6 +3,7 @@ import 'package:app/widgets/button/common_text_button.dart';
 import 'package:app/widgets/dialog/dialog.dart';
 import 'package:app/widgets/header/header.dart';
 import 'package:app/widgets/inputbox/common_smallinputbox.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -19,64 +20,77 @@ class _ChangePasswordState extends State<ChangePassword> {
     final viewModel = Provider.of<PasswordChangeViewModel>(context);
     return Scaffold(
       appBar: const Header(title: '비밀번호 재설정', back: true),
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: Column(
-            children: [
-              SizedBox(height: 80,),
-              Text('비밀번호는 영문 대문자/소문자/숫자/특수문자를 섞어 8~16자리로 입력해주세요.'),
-              SizedBox(height: 20,),
-              SmallInputBox(
-                  hintText: '새 비밀번호',
-                  textInputAction: TextInputAction.next,
-                  obscureText: true,
-                  onChanged: (value) {
-                    viewModel.setNewPwd(value);
-                  }),
-              SizedBox(height: 20,),
-              SmallInputBox(
-                  hintText: '새 비밀번호 확인',
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                  onChanged: (value) {
-                    viewModel.setCheckNewPwd(value);
-                  }),
-              SizedBox(height: 50,),
-              CommonTextButton(
-                  text: '확인',
-                  onTap: () {
-                    viewModel.passwordChange().then((_) {
-                      if (viewModel.errorMessage == null) {
-                        showDialog(
-                            context: context,
-                            barrierDismissible: false,
-                            builder: (BuildContext context) {
-                              return CommonDialog(
-                                content: '비밀번호 변경이 완료되었습니다.\n다시 로그인 해주세요.',
-                                buttonText: '확인',
-                                buttonClick: () {
-                                  Navigator.pushNamedAndRemoveUntil(
-                                      context, '/login', (route) => false);
-                                },
-                              );
-                            });
-                      } else {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return CommonDialog(
-                                content: viewModel.errorMessage!,
-                                buttonText: '확인',
-                              );
-                            });
-                      }
-                    });
-                  }),
-              SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
-            ],
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Column(
+                  children: [
+                    SizedBox(height: 50,),
+                    Text('비밀번호는 영문 대문자/소문자/숫자/특수문자를 섞어 8~16자리로 입력해주세요.'),
+                    SizedBox(height: 30,),
+                    SmallInputBox(
+                        hintText: '새 비밀번호',
+                        textInputAction: TextInputAction.next,
+                        obscureText: true,
+                        onChanged: (value) {
+                          viewModel.setNewPwd(value);
+                        }),
+                    SizedBox(height: 20,),
+                    SmallInputBox(
+                        hintText: '새 비밀번호 확인',
+                        textInputAction: TextInputAction.done,
+                        obscureText: true,
+                        onChanged: (value) {
+                          viewModel.setCheckNewPwd(value);
+                        }),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: CommonTextButton(
+                text: '확인',
+                onTap: () {
+                  viewModel.passwordChange().then((_) {
+                    if (viewModel.errorMessage == null) {
+                      showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (BuildContext context) {
+                            return CommonDialog(
+                              content: '비밀번호 변경이 완료되었습니다.\n다시 로그인 해주세요.',
+                              buttonText: '확인',
+                              buttonClick: () {
+                                Navigator.pushNamedAndRemoveUntil(
+                                    context, '/login', (route) => false);
+                              },
+                            );
+                          });
+                    } else {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return CommonDialog(
+                              content: viewModel.errorMessage!,
+                              buttonText: '확인',
+                            );
+                          });
+                    }
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
