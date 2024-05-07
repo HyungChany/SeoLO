e5676070144d1b1f563c427fa5330470ed679b85
오정민 <dhwjdals1324@gmail.com>
2024-05-07 12:17:49 +0900

    feat : employeeregistration api 코드 작성

--- frontend/web/src/apis/Employee.ts
+++ frontend/web/src/apis/Employee.ts
@@ -1,5 +1,9 @@
 import { api } from './Base.ts';
-
+interface EmployeeType {
+  username: string;
+  password: string;
+  company_code: string;
+}
 export const EmployeeList = async () => {
   try {
     const accessToken = sessionStorage.getItem('accessToken');
@@ -21,6 +25,22 @@ export const EmployeeDetail = async (employeeNumber: string) => {
     const accessToken = sessionStorage.getItem('accessToken');
     const companyCode = sessionStorage.getItem('companyCode');
     const response = await api.get(`/employees/${employeeNumber}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Company-Code': companyCode,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const EmployeeRegistration = async (employeeData: EmployeeType) => {
+  try {
+    const accessToken = sessionStorage.getItem('accessToken');
+    const companyCode = sessionStorage.getItem('companyCode');
+    const response = await api.post(`/join`, employeeData, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         'Company-Code': companyCode,
