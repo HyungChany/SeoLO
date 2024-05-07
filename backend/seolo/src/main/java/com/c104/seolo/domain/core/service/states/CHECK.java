d51f22a5558a9b3eaba728925b69b0fc2f6e9d30
김진명 <willom2c@naver.com>
2024-05-07 10:17:58 +0900

    [BE] feat : Create find a latest task history by machineId

--- backend/seolo/src/main/java/com/c104/seolo/domain/core/service/states/CHECK.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/core/service/states/CHECK.java
@@ -10,6 +10,18 @@ public class CHECK implements CodeState {
 
     @Override
     public CoreResponse handle(Context context) {
+        /*
+        행동코드 모듈 CHECK
+        작업내역 정보를 응답한다.
+
+        1. 장비ID를 이용해 작업내역TB 의 데이터 중 가장 최신 튜플을 조회한다.
+        2. 전달받은 유저 SESSION 정보를 통해 자물쇠에 접근한 유저 정보를 DB에 기록한다.
+        3. 1번에서 조회한 작업내역 데이터를 응답한다.
+        */
+
+
+
+
         return null;
     }
 }
