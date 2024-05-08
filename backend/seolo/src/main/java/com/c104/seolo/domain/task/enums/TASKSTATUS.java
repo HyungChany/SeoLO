101ce33bad07f483c85d205158a9e29af67aef20
김진명 <willom2c@naver.com>
2024-05-09 00:11:40 +0900

    [BE] feat : Creat Report Entity

--- backend/seolo/src/main/java/com/c104/seolo/domain/task/enums/TASKSTATUS.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/task/enums/TASKSTATUS.java
@@ -3,12 +3,12 @@ package com.c104.seolo.domain.task.enums;
 import lombok.Getter;
 
 @Getter
-public enum TaskStatus {
+public enum TASKSTATUS {
     ING("작업중"),
     END("작업완료");
 
     private final String selectTaskStatus;
-    TaskStatus(String selectTaskStatus) {
+    TASKSTATUS(String selectTaskStatus) {
         this.selectTaskStatus = selectTaskStatus;
     }
 }
--- backend/seolo/src/main/java/com/c104/seolo/domain/task/enums/TASKTYPE.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/task/enums/TASKTYPE.java
@@ -3,14 +3,14 @@ package com.c104.seolo.domain.task.enums;
 import lombok.Getter;
 
 @Getter
-public enum TaskType {
+public enum TASKTYPE {
     FIX("수리"),
     REPAIR("정비"),
     CLEAN("청소"),
     ETC("기타");
 
     private final String selectTaskType;
-    TaskType(String selectTaskType) {
+    TASKTYPE(String selectTaskType) {
         this.selectTaskType = selectTaskType;
     }
 }
