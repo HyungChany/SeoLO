2063ffac97c2f26088db327ad4ed3ca2d15b9de6
오민상 <flos9537@gmail.com>
2024-05-08 11:38:56 +0900

    [FE] chore: import문 정리

--- frontend/watch/app/src/main/java/com/seolo/seolo/presentation/MainActivity.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/presentation/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.seolo.seolo.R
 import com.seolo.seolo.databinding.ActivityMainBinding
 import com.seolo.seolo.fragments.MainFragment
 
