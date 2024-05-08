60bddea79fa1a1337c05b5ce3e9f696d6fea8a31
오민상 <flos9537@gmail.com>
2024-05-08 23:58:48 +0900

    [FE] feat: http 요청 응답 디테일 로그 확인 의존성 추가 및 관련 코드 추가

--- frontend/watch/app/src/main/java/com/seolo/seolo/services/RetrofitClient.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/services/RetrofitClient.kt
@@ -1,14 +1,25 @@
 package com.seolo.seolo.services
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://k10c104.p.ssafy.io/api/"
 
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
