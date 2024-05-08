1d7ab4aea258853bc2087254ee792ead93c0bdbc
김진명 <willom2c@naver.com>
2024-05-08 09:21:39 +0900

    [BE] fix : sovling merging confilct

--- backend/seolo/src/main/java/com/c104/seolo/domain/task/repository/TaskHistoryRepository.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/task/repository/TaskHistoryRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Long> {
     @Query("SELECT new com.c104.seolo.domain.task.dto.info.TaskHistoryInfo( " +
             "t.id, tt.taskType, t.taskStartDateTime, t.taskEndDateTime, t.taskEndEstimatedDateTime, " +
-            "t.taskPrecaution, t.taskStatus, u.employee.company.companyCode, " +
+            "t.taskPrecaution, u.employee.company.companyCode, " +
             " m.facility.facilityName, m.name, m.number, " +
             "u.employee.employeeTeam, u.employee.employeeName, u.employee.employeeTitle" +
             ") FROM TaskHistory t JOIN t.taskTemplate tt JOIN t.machine m JOIN t.user u " +
@@ -26,12 +26,9 @@ public interface TaskHistoryRepository extends JpaRepository<TaskHistory, Long> 
     @Query("SELECT t FROM TaskHistory t WHERE (t.machine.id = :machineId OR t.user.id = :userId) AND t.taskCode is NOT NULL")
     Optional<TaskHistory> findByMachineIdAndUserIdOrTaskCode(Long machineId, Long userId);
 
-    @Query("SELECT t FROM TaskHistory t WHERE t.machine.id = :machineId ORDER BY t.taskStartDateTime DESC")
-    Optional<TaskHistory> getTaskHistoryByMachineId(Long machineId);
-
     @Query("SELECT new com.c104.seolo.domain.task.dto.info.TaskHistoryInfo( " +
             "t.id, t.taskTemplate.taskType, t.taskStartDateTime, t.taskEndDateTime, t.taskEndEstimatedDateTime, " +
-            "t.taskPrecaution, t.taskStatus, u.employee.company.companyCode, " +
+            "t.taskPrecaution, u.employee.company.companyCode, " +
             " m.facility.facilityName, m.name, m.number, " +
             "u.employee.employeeTeam, u.employee.employeeName, u.employee.employeeTitle " +
             ") FROM TaskHistory t join t.user u join t.machine m where t.user.employee.employeeNum = :employeeNum order by t.taskStartDateTime desc ")
--- backend/seolo/src/main/java/com/c104/seolo/domain/task/service/TaskHistoryService.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/task/service/TaskHistoryService.java
@@ -1,10 +1,17 @@
 package com.c104.seolo.domain.task.service;
 
+import com.c104.seolo.domain.task.dto.TaskHistoryDto;
+import com.c104.seolo.domain.task.dto.response.TaskHistoryResponse;
 import com.c104.seolo.domain.task.dto.response.TaskListResponse;
-import com.c104.seolo.domain.task.entity.TaskHistory;
+import com.c104.seolo.global.security.jwt.entity.CCodePrincipal;
 
 public interface TaskHistoryService {
-    TaskHistoryDto getTaskHistory(Long taskId, String companyCode);
-    TaskHistory getLatestTaskHistoryEntityByMachineId(Long machineId);
+    TaskHistoryResponse getTaskHistory(Long taskId, String companyCode);
+    TaskHistoryDto getLatestTaskHistoryEntityByMachineId(Long machineId);
     TaskListResponse getTaskHistoryEntityByEmployeeNum(String employeeNum);
+    void enrollTaskHistory(CCodePrincipal cCodePrincipal,
+                           Long taskTemplateId,
+                           Long machineId,
+                           String endTime,
+                           String taskPrecaution);
 }
