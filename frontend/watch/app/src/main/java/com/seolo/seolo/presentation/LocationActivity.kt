02ec304f38db0386d3efc67383ffd7086e232dbc
오민상 <flos9537@gmail.com>
2024-05-09 17:16:04 +0900

    feat: 데이터 직렬화 기능 추가

--- frontend/watch/app/src/main/java/com/seolo/seolo/presentation/LocationActivity.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/presentation/LocationActivity.kt
@@ -6,28 +6,29 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.seolo.seolo.R
 import com.seolo.seolo.adapters.WheelPickerAdapter
+import com.seolo.seolo.model.FacilityItem
 import sh.tyy.wheelpicker.core.WheelPickerRecyclerView
 
 // LocationActivity 클래스 정의
 class LocationActivity : AppCompatActivity() {
-    private lateinit var locations: List<String>
+    private lateinit var facilities: List<FacilityItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.basic_wheel_picker_layout)
 
-        // 인텐트에서 facilities 정보 받기
-        val facilities = intent.getStringArrayListExtra("facilities") ?: listOf()
-        locations = listOf(" ") + facilities + " "
+        // 인텐트에서 facilities 정보를 Parcelable로 받기
+        facilities = intent.getParcelableArrayListExtra<FacilityItem>("facilities") ?: arrayListOf()
+        val facilityNames = listOf(" ") + facilities.map { it.name } + " "
 
 
         val locationPicker = findViewById<WheelPickerRecyclerView>(R.id.basic_wheel_picker_view)
-        val locationAdapter = WheelPickerAdapter(locations)
+        val locationAdapter = WheelPickerAdapter(facilityNames)
         locationPicker.adapter = locationAdapter
 
         locationPicker.post {
-            val middlePosition = locations.size / 2
+            val middlePosition = facilityNames.size / 2
             locationPicker.layoutManager?.scrollToPosition(middlePosition)
         }
 
