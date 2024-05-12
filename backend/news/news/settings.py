d4efabcc603b00667f3b3508aac72e55394413a3
김진명 <willom2c@naver.com>
2024-05-12 12:34:55 +0900

    [DEV] fix : modify my_setting import url

--- backend/news/news/settings.py
+++ backend/news/news/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from setting.my_setting.py import *
+from setting.my_setting import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
