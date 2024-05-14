6d7954e458455d3dad7cdf8891edec3e702669fe
오민상 <flos9537@gmail.com>
2024-05-14 21:36:29 +0900

    fix: 레이아웃 id 참조 관련 err 수정

--- frontend/watch/app/src/main/java/com/seolo/seolo/presentation/BluetoothPickActivity.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/presentation/BluetoothPickActivity.kt
@@ -51,7 +51,7 @@ class BluetoothPickActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         // RecyclerView 초기화 및 설정
-        recyclerView = findViewById(R.id.bluetoothDeviceRecyclerView)
+        recyclerView = findViewById(R.id.bluetoothListItem)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         // RecyclerView 어댑터 설정
