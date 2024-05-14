fa954d61f0d850961a63e0e3c2189141ac7ea2f7
오민상 <flos9537@gmail.com>
2024-05-14 13:17:59 +0900

    chore: 주석 수정

--- frontend/watch/app/src/main/java/com/seolo/seolo/presentation/BluetoothActivity.kt
+++ frontend/watch/app/src/main/java/com/seolo/seolo/presentation/BluetoothActivity.kt
@@ -28,6 +28,7 @@ import java.nio.charset.StandardCharsets
 import java.util.UUID
 
 class BluetoothActivity : AppCompatActivity() {
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private lateinit var deviceAdapter: BluetoothDeviceAdapter
@@ -75,6 +76,7 @@ class BluetoothActivity : AppCompatActivity() {
         }
     }
 
+    // 기기 선택 시 호출되는 함수
     @RequiresApi(Build.VERSION_CODES.S)
     private fun onDeviceSelected(device: BluetoothDevice) {
         // 기기 선택 시 GATT 스캐닝 중지
@@ -89,6 +91,7 @@ class BluetoothActivity : AppCompatActivity() {
     // 기기 연결 및 데이터 전송 로직을 포함한 함수
     @RequiresApi(Build.VERSION_CODES.S)
     private fun connectToDevice(device: BluetoothDevice) {
+        // Bluetooth 연결 권한 확인
         if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
             val deviceName = device.name ?: "Unknown Device"
             Toast.makeText(this@BluetoothActivity, "$deviceName 클릭", Toast.LENGTH_SHORT).show()
@@ -105,6 +108,8 @@ class BluetoothActivity : AppCompatActivity() {
 
     // GATT 콜백 정의
     private val gattClientCallback = object : BluetoothGattCallback() {
+
+        // 연결 상태 변경 시 호출되는 콜백
         @RequiresApi(Build.VERSION_CODES.S)
         override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
             super.onConnectionStateChange(gatt, status, newState)
@@ -138,12 +143,12 @@ class BluetoothActivity : AppCompatActivity() {
             }
         }
 
+        // 서비스가 발견되었을 때 호출되는 콜백
         @RequiresApi(Build.VERSION_CODES.S)
         override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
             super.onServicesDiscovered(gatt, status)
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                // 서비스가 발견되었을 때
-                // 적절한 서비스와 캐릭터리스틱 찾기
+                // 서비스가 성공적으로 발견되었을 때
                 val service = gatt?.getService(SERVICE_UUID) // 해당 서비스의 UUID
                 val char = service?.getCharacteristic(CHAR_UUID) // 쓰기 위한 캐릭터리스틱의 UUID
 
@@ -177,14 +182,15 @@ class BluetoothActivity : AppCompatActivity() {
             }
         }
 
+        // 캐릭터리스틱에 데이터가 성공적으로 쓰였을 때 호출되는 콜백
         override fun onCharacteristicWrite(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int) {
             super.onCharacteristicWrite(gatt, characteristic, status)
             if (status == BluetoothGatt.GATT_SUCCESS) {
-                // 캐릭터리스틱에 데이터가 성공적으로 쓰였을 때
                 Log.d("BluetoothActivity1", "${characteristic?.value?.toString(StandardCharsets.UTF_8)}")
             }
         }
 
+        // 캐릭터리스틱 값이 변경되었을 때 호출되는 콜백
         override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
             super.onCharacteristicChanged(gatt, characteristic)
             // 아두이노에서 보내온 데이터 수신
