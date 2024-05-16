3b0bf13153247f7fedf318a00798ab4bac65cb4c
김진명 <willom2c@naver.com>
2024-05-16 20:52:24 +0900

    [BE] feat : exception when add same machine marker

--- backend/seolo/src/main/java/com/c104/seolo/domain/marker/exception/MarkerErrorCode.java
+++ backend/seolo/src/main/java/com/c104/seolo/domain/marker/exception/MarkerErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MarkerErrorCode {
-    NOT_EXIST_MARKER("MR01", "해당 마커를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    NOT_EXIST_MARKER("MR01", "해당 마커를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    ALREADY_ENROLLED_MACHINE("MR02", "이미 마커로 등록된 장비입니다.", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
